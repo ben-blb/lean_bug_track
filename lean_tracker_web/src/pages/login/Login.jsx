af49c82f4def3e00647403d2c85e8aba9ff11b75
= <=>
2022-03-23 00:08:37 -0600

    Added webapp and started login page

---
+++ lean_tracker_web/src/pages/login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Login.css'
+
+export default function Login() {
+  return (
+    <main className="login">
+        <section>
+            <h1>Lean</h1><br></br>
+            <h1>Bug</h1><br></br>
+            <h1>Track</h1><br></br>
+            <hr />
+            <button>Entrar con Google</button>
+        </section>
+    </main>
+  )
+}
